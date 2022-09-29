0d79d03da3148f00aabd9e5929c5a9c367e122bc
Javi <javiermoyasimo@gmail.com>
2022-09-30 01:33:32 +0200

    finished and linked index

---
+++ javascript/reservar.js
@@ -0,0 +1,10 @@
+/*var button = document.getElementById('reservar');
+
+
+function phone() {
+    alert("Llamar al 652888444")
+    }
+
+
+// Evento click para el boton Reservar con Id('reservar')
+button.addEventListener('click', phone);*/
